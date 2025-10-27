afc29e75178b50e74dca8ab388b327648ed60ff1
taki <takiedd.mekhalfa@gmail.com>
2025-10-27 17:56:00 +0100

    feat: Add text styles

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'package:examen_civique/style/app_colors.dart';
+import 'package:examen_civique/style/app_text_styles.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:intl/intl.dart';
 
 void main() {
@@ -17,8 +20,22 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Mon Examen Civique',
       locale: const Locale('fr', 'FR'),
+      localizationsDelegates: const [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate, // 👈 include this one too
+        GlobalCupertinoLocalizations.delegate,
+      ],
       supportedLocales: const [Locale('en', 'US'), Locale('fr', 'FR')],
-      home: const Scaffold(body: Center(child: Text('Bonjour!'))),
+      theme: ThemeData(
+        colorScheme: ColorScheme.light(
+          primary: AppColors.primaryBlue,
+          secondary: AppColors.primaryBlue,
+        ),
+        hoverColor: AppColors.neutral200,
+        highlightColor: AppColors.neutral200,
+        focusColor: AppColors.neutral200,
+      ),
+      home: const Scaffold(body: Center(child: Text('Bonjour!', style: AppTextStyles.regular50))),
       debugShowCheckedModeBanner: false,
     );
   }
