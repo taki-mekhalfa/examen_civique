9b7efba614e805392fec92a1cab665debb587197
taki <takiedd.mekhalfa@gmail.com>
2025-11-27 22:29:21 +0100

    fix : solve timestracker race condition

--- lib/app_time_tracker.dart
+++ lib/app_time_tracker.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:developer' as developer;
 
 import 'package:examen_civique/data/app_db.dart';
 import 'package:examen_civique/repositories/repository.dart';
@@ -6,30 +7,50 @@ import 'package:flutter/material.dart';
 
 class AppTimeTracker with WidgetsBindingObserver {
   AppTimeTracker._();
-  static final AppTimeTracker instance = AppTimeTracker._()..init();
+  static final AppTimeTracker instance = AppTimeTracker._();
   factory AppTimeTracker() => instance;
 
   static const _interval = Duration(minutes: 1);
 
   late DateTime _startTime;
-  late Timer _timer;
+  Timer? _timer;
 
   void init() {
     WidgetsBinding.instance.addObserver(this);
     _startTimer();
   }
 
-  void _startTimer() {
-    _startTime = DateTime.now();
-    _timer = Timer.periodic(_interval, (_) => _flush());
+  void dispose() {
+    WidgetsBinding.instance.removeObserver(this);
+    _timer?.cancel();
   }
 
-  Future<void> _flush() async {
+  void _startTimer() {
+    if (_timer != null && _timer!.isActive) return;
+
+    _startTime = DateTime.now();
+    _timer = Timer.periodic(_interval, (_) => _flush(resetStartTime: true));
+  }
+
+  Future<void> _flush({required bool resetStartTime}) async {
     final now = DateTime.now();
+    if (now.isBefore(_startTime)) {
+      _startTime = now;
+      return;
+    }
+
     final elapsed = now.difference(_startTime);
-    final db = await AppDb.instance.database;
-    await Repository(db: db).updateTimeSpentStats(now, elapsed);
-    _startTime = now;
+
+    if (resetStartTime) {
+      _startTime = now;
+    }
+
+    try {
+      final db = await AppDb.instance.database;
+      await Repository(db: db).updateTimeSpentStats(now, elapsed);
+    } catch (e) {
+      developer.log("Erreur sauvegarde temps: $e");
+    }
   }
 
   @override
@@ -38,9 +59,12 @@ class AppTimeTracker with WidgetsBindingObserver {
       case AppLifecycleState.resumed:
         _startTimer();
         break;
-      case AppLifecycleState.paused: // backgrounded
-        _flush();
-        _timer.cancel();
+
+      case AppLifecycleState.inactive:
+      case AppLifecycleState.paused:
+      case AppLifecycleState.detached:
+        _flush(resetStartTime: false);
+        _timer?.cancel();
         break;
       default:
         break;
